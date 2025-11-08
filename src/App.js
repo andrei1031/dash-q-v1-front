01dfab4dc8b98f284b387a37b83d57c0999b2116
Reuben Andrei Saldivar <andreisantossaldivar@gmail.com>
2025-11-08 20:14:17 +0800

    notification sound

--- src/App.js
+++ src/App.js
@@ -621,7 +621,7 @@ function CustomerView({ session }) {
    const [services, setServices] = useState([]);
    const [selectedServiceId, setSelectedServiceId] = useState('');
    const [isChatOpen, setIsChatOpen] = useState(false);
-   const [chatTargetBarberUserId, setChatTargetBarberUserId] = useState(null);
+   const [_chatTargetBarberUserId, setChatTargetBarberUserId] = useState(null);
    const [isYourTurnModalOpen, setIsYourTurnModalOpen] = useState(false);
    const [isServiceCompleteModalOpen, setIsServiceCompleteModalOpen] = useState(false);
    const [isCancelledModalOpen, setIsCancelledModalOpen] = useState(false);
